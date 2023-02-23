a27e9e4aca5251115e07f1aa1e727f9d5ec79c40
TheTurkeyDev <turkey@theturkey.dev>
2023-02-23 12:52:42 -0500

    Added missing exports

--- src/payloads/index.ts
+++ src/payloads/index.ts
@@ -1,4 +1,5 @@
 export * from './gateway-payload';
+export * from './guild-request-members';
 export * from './heartbeat-payload';
 export * from './identify-payload';
 export * from './resume-payload';
